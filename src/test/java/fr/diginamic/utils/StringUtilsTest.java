f3f361febbf1735ece032451ddcc52d000d9fd31
rijarandria <rijandrisolo@gmail.com>
2023-11-22 17:29:54 +0100

    First commit test DISTANCE DE LEVENSHTEIN v2.0

--- src/test/java/fr/diginamic/utils/StringUtilsTest.java
+++ src/test/java/fr/diginamic/utils/StringUtilsTest.java
@@ -13,9 +13,9 @@ import static org.junit.jupiter.api.Assertions.*;
   void levenshteinDistance(){
    int res = StringUtils.levenshteinDistance("Chien","chine");
   // int res1 = StringUtils.levenshteinDistance("machins","machine");
-  // int res1 = StringUtils.levenshteinDistance("distance","instance");
+   int res1 = StringUtils.levenshteinDistance("distance","instance");
     //assertEquals(res,1);
-   // assertEquals(res1,1);
+    assertEquals(res1,1);
 
   }
 
