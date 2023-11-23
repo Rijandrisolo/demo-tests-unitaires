1bfbe674e64ff35200b6790cc7ec42de0779fe59
rijarandria <rijandrisolo@gmail.com>
2023-11-23 20:48:16 +0100

    First commit test DISTANCE DE LEVENSHTEIN v2.0

--- src/test/java/fr/diginamic/immobilier/entites/MaisonTest.java
+++ src/test/java/fr/diginamic/immobilier/entites/MaisonTest.java
@@ -1,7 +1,37 @@
 package fr.diginamic.immobilier.entites;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MaisonTest {
+    @Test
+    void ajouterPieceTest(Piece nvPiece){
 
-}
+        Piece[] pieces = new Piece[10];
+        int nbPieces = pieces.length;
+        // On est obligé d'agrandir le tableau initial de 1 à chaque ajout
+        // d'une nouvelle pièce
+
+        // On commence donc par créer un tableau temporaire appelé newTab
+        // qui a une taille égale à la tableau du tableau pieces+1
+        Piece[] newTab = new Piece[pieces.length+1];
+
+        // On déverse toutes les pièces du tableau pieces dans newTab
+        for (int i=0; i<pieces.length; i++){
+            newTab[i]=pieces[i];
+        }
+
+        // On place en dernière position dans le nouveau tableau la nouvelle
+        // pièce
+        newTab[newTab.length-1]=nvPiece;
+            int newNbPieces = newTab.length;
+        // Enfin on affecte newTab à pieces
+        //this.pieces=newTab;
+
+          // assertFalse(nbPieces>newNbPieces);
+
+    }
+
+    }
+
--- src/test/java/fr/diginamic/utils/StringUtilsTest.java
+++ src/test/java/fr/diginamic/utils/StringUtilsTest.java
@@ -8,14 +8,18 @@ import static org.junit.jupiter.api.Assertions.*;
  class StringUtilsTest {
 
 
- StringUtils stringUtils =new StringUtils();
+ //StringUtils stringUtils =new StringUtils();
   @Test
   void levenshteinDistance(){
-   int res = StringUtils.levenshteinDistance("Chien","chine");
-  // int res1 = StringUtils.levenshteinDistance("machins","machine");
-   int res1 = StringUtils.levenshteinDistance("distance","instance");
-    //assertEquals(res,1);
-    assertEquals(res1,1);
+   //int res = StringUtils.levenshteinDistance("Chien","chine");
+   //int res1 = StringUtils.levenshteinDistance("machins","machine");
+   int res2 = StringUtils.levenshteinDistance("avirons","avion");
+   int res3 = StringUtils.levenshteinDistance("distance","instance");
+   //assertEquals(res,1);
+   //assertEquals(res1,1);
+    assertEquals(res2,2);
+   //assertEquals(res2,2);
+
 
   }
 
